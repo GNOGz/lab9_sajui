8944805bdb2b09968d4d0bc731ec221833ee51a3
GNOGz <gong6321@gmail.com>
2023-12-15 14:53:22 +0700

    complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 //Write the function printO() here
-
+void printO(int row,int column){
+if(row <= 0 || column <= 0){
+	cout <<"Invalid input";
+}
+else {
+	for(int i = 1 ; i<= row ; i++){
+		for(int j = 1 ; j <= column; j++){
+			cout<<"O";
+		}
+		cout << "\n" ;
+	}
+}
+}
 int main(){
 	
 	printO(2,2);
